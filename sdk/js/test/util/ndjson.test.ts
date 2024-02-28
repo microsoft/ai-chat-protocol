d263ebdf45fd40273d2860a4cf50286e69c01ea4
Gerardo Lecaros <gerardo.lecaros.e@gmail.com>
2024-02-28 01:02:07 +0000

    More tests

--- sdk/js/test/util/ndjson.test.ts
+++ sdk/js/test/util/ndjson.test.ts
@@ -1,5 +1,6 @@
 import { expect, test, vi } from "vitest";
 import { getAsyncIterable } from "../../src/util/ndjson.js";
+import exp from "constants";
 
 function getReadableStream(lines: string[]) {
   return new ReadableStream({
@@ -51,4 +52,30 @@ test("Can parse file delimited with mixed '\\n' and '\\r\\n'", async () => {
     values.push(value);
   }
   expect(values).toEqual([{ a: 1 }, { a: 2 }, { a: 3 }]);
+});
+
+test("Can parse file with empty lines", async () => {
+  const stream = getReadableStream(['{"a":1}\n\n{"a":2}\n\n{"a":3}\n']);
+  const iterable = await getAsyncIterable<Foo>(stream);
+
+  let values: Foo[] = [];
+  for await (const value of iterable) {
+    values.push(value);
+  }
+  expect(values).toEqual([{ a: 1 }, { a: 2 }, { a: 3 }]);
+});
+
+test("When received a malformed JSON, it throws an error", async () => {
+  const stream = getReadableStream(['{"a":1}\n{', '"a":2}\n{"', 'a":3\n']);
+  const iterable = await getAsyncIterable<Foo>(stream);
+
+  let values: Foo[] = [];
+  try {
+    for await (const value of iterable) {
+      values.push(value);
+    }
+    expect(false).toBe(true);
+  } catch (e) {
+    expect(e).toBeInstanceOf(SyntaxError);
+  }
 });
