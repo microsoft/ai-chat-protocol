ba87a1ad373c9daacfc61b52e487400a0208bf8b
Gerardo Lecaros <gerardo.lecaros.e@gmail.com>
2024-03-12 22:31:29 +0000

    format + title

--- samples/chat-sample-react/src/App.tsx
+++ samples/chat-sample-react/src/App.tsx
@@ -36,8 +36,13 @@ const useMessageStyles = makeStyles({
 });
 
 function Chat() {
-  const client = new AIChatProtocolClient('http://localhost:8080/chat', {allowInsecureConnection: true});
-  const configMessage: AIChatMessage = { role: 'system', content: 'You are an enthusiastic assistant that talks like a 5 year old.' };
+  const client = new AIChatProtocolClient('http://localhost:8080/chat', {
+    allowInsecureConnection: true
+  });
+  const configMessage: AIChatMessage = {
+    role: 'system',
+    content: 'You are an enthusiastic assistant that talks like a 5 year old.',
+  };
 
   const [messages, setMessages] = useState<AIChatMessage[]>([]);
   const [input, setInput] = useState<string>('');
@@ -58,21 +63,21 @@ function Chat() {
     setMessages(updatedMessages);
     setInput('');
     if (streaming) {
-    const result = await client.getStreamedCompletion([configMessage, ...updatedMessages]);
-    const latestMessage: AIChatMessage = { content: "", role: 'assistant'};
-    for await (const response of result) {
-      const choice = response.choices[0];
-      if (!choice.delta) {
-        continue;
+      const result = await client.getStreamedCompletion([configMessage, ...updatedMessages]);
+      const latestMessage: AIChatMessage = { content: "", role: 'assistant' };
+      for await (const response of result) {
+        const choice = response.choices[0];
+        if (!choice.delta) {
+          continue;
+        }
+        if (choice.delta.role) {
+          latestMessage.role = choice.delta.role;
+        }
+        if (choice.delta.content) {
+          latestMessage.content += choice.delta.content;
+          setMessages([...updatedMessages, latestMessage]);
+        }
       }
-      if (choice.delta.role) {
-        latestMessage.role = choice.delta.role;
-      }
-      if (choice.delta.content) {
-        latestMessage.content += choice.delta.content;
-        setMessages([...updatedMessages, latestMessage]);
-      }
-    }
     } else {
       const result = await client.getCompletion([configMessage, ...updatedMessages]);
       setMessages([...updatedMessages, result.choices[0].message]);
@@ -83,19 +88,19 @@ function Chat() {
   const messageStyles = useMessageStyles();
   return (
     <div>
-        <div className={styles.messages}>
-          {messages.map(message => (
-            <div key={crypto.randomUUID()}>
-              <Text className={ message.role === 'user' ? messageStyles.user : messageStyles.assistant}>{message.role}: {message.content}</Text>
-            </div>
-          ))}
-        </div>
-        <div className={styles.input}>
-        <Input id={inputId} value={input} onChange={(e) => setInput(e.target.value) }/>
+      <div className={styles.messages}>
+        {messages.map(message => (
+          <div key={crypto.randomUUID()}>
+            <Text className={message.role === 'user' ? messageStyles.user : messageStyles.assistant}>{message.role}: {message.content}</Text>
+          </div>
+        ))}
+      </div>
+      <div className={styles.input}>
+        <Input id={inputId} value={input} onChange={(e) => setInput(e.target.value)} />
         <Button onClick={sendMessage}>Send</Button>
         <Button onClick={clear}>Clear</Button>
         <ToggleButton checked={streaming} onClick={(e) => setStreaming(!streaming)} >Streaming</ToggleButton>
-        </div>
+      </div>
     </div>
   );
 }
