0aeb6046c62425212e89441f05a7db562fa4a97c
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2024-03-20 15:04:14 -0700

    Restore default option in example-curl-client-chat.cmd

--- extra/test-server/example-curl-client-chat.cmd
+++ extra/test-server/example-curl-client-chat.cmd
@@ -19,9 +19,9 @@ IF "%~1"=="" (
 )
 
 REM Good for testing non-streaming:
-curl -v "https://localhost:%1/chat" -N -H "Content-Type: application/json" --data-ascii "{\"messages\": [{\"role\":\"system\",\"content\":\"You are an AI assistant that helps people find information.\"},{\"role\":\"user\",\"content\":\"How many feet in a mile??\"}]}"
+REM curl -v "https://localhost:%1/chat" -N -H "Content-Type: application/json" --data-ascii "{\"messages\": [{\"role\":\"system\",\"content\":\"You are an AI assistant that helps people find information.\"},{\"role\":\"user\",\"content\":\"How many feet in a mile??\"}]}"
 REM curl -v "https://localhost:%1/chat" -H "Content-Type: application/json" --data-ascii "{\"messages\": [{\"role\":\"system\",\"content\":\"You are a helpful assistant. You will talk like a pirate.\"},{\"role\":\"user\",\"content\":\"Can you help me?\"},{\"role\":\"assistant\",\"content\":\"Arrrr! Of course, me hearty! What can I do for ye?\"},{\"role\":\"user\",\"content\":\"What's the best way to train a parrot?\"}]}"
 
 
 REM Good for testing streaming (long response):
-REM curl -v "https://localhost:%1/chat/stream" -N -H "Content-Type: application/json" --data-ascii "{\"messages\": [{\"role\":\"system\",\"content\":\"You are an AI assistant that helps people find information.\"},{\"role\":\"user\",\"content\":\"Give me ten reasons to regularly exercise.\"}]}"
+curl -v "https://localhost:%1/chat/stream" -N -H "Content-Type: application/json" --data-ascii "{\"messages\": [{\"role\":\"system\",\"content\":\"You are an AI assistant that helps people find information.\"},{\"role\":\"user\",\"content\":\"Give me ten reasons to regularly exercise.\"}]}"
