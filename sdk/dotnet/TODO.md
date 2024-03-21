74a01e8ab7d5711a38c8dbf23588bdc26edc1883
Darren Cohen <39422044+dargilco@users.noreply.github.com>
2024-03-21 12:14:53 -0700

    First drop of C# client library

---
+++ sdk/dotnet/TODO.md
@@ -0,0 +1,37 @@
+# List of work items
+
+## Contacts
+
+- For anything related to .NET System.ClientModel (SCM) contact Anne Loomis Thompson.
+
+## SDK
+
+- Test against a service that uses token authentication (see ApiKeyCredential in client constructor)
+- Should I use the serialization/deserialization provided with System.ClientModel?
+  (see https://github.com/Azure/azure-sdk-for-net/blob/863cec65455e1f54fa9d3131c779d1f1c6072174/sdk/core/System.ClientModel/README.md).
+- Support `session_state` and `context` fields - Decide what object type they are (at the moment they are strings). Add tests.
+- Use class names that are different than AOAI SDK and the new unbranded OAI SDK
+- Decide on name space (use Microsoft.AI.Chat instead of Microsoft.AI.ChatProtocl?)
+- Should there be an easy way to get read-only HTTP request details using public APIs, like we have in Java?. The alternative is to use policy. Should I expose HttpResponseMessage to the caller? I had a conversation with Anne on this. I need this to validate test results and I think developers will also want read-access to request details. When I updated the test app to get chatCompletion.Response.RequestMessage?.Content?.ReadAsStringAsync().Result I got an exception saying the Content was already disposed. Should I just expose response status code, response headers and raw JSON as separate properties?
+
+## Unit testing
+
+### Error tests
+
+- Test parsing invalid JSON (null/missing JSON element). Validate the correct exception is thrown.
+- Test parsing empty JSON lines in the response (we should silently skip them)
+
+## Functional (end-to-end) testing
+
+- Test overriding default User-Agent header
+- Test that on service-originated-error, an exception is thrown and the error message contains HTTP status code and message from service
+- Test service-originated-error mid way during response streaming (see [here](https://github.com/Azure-Samples/ai-chat-app-protocol?tab=readme-ov-file#error-response-1))
+
+## Sample code
+
+
+## Ref docs
+
+- Script to convert XML to HTML using docfx
+
+## NuGet package
---
+++ sdk/dotnet/src/ChatMessage.cs
@@ -0,0 +1,83 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE file in the project root for license information.
+
+namespace Microsoft.AI.ChatProtocol
+{
+    using System.Text.Json;
+
+    /// <summary>
+    /// A single, role-attributed message within a chat completion interaction.
+    /// </summary>
+    public class ChatMessage : IUtf8JsonSerializable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ChatMessage"/> class.
+        /// </summary>
+        /// <param name="role"> The role associated with the message. </param>
+        /// <param name="content"> The message content. </param>
+        public ChatMessage(ChatRole role, string content)
+        {
+            if (content is null)
+            {
+                throw new ArgumentNullException(nameof(content));
+            }
+
+            this.Role = role;
+            this.Content = content;
+        }
+
+        /// <summary>
+        /// Gets or sets the role associated with the message.
+        /// </summary>
+        public ChatRole Role { get; set; }
+
+        /// <summary>
+        /// Gets or sets the text of the message.
+        /// </summary>
+        public string Content { get; set; }
+
+        /// <summary>
+        /// A string representation of the ChatMessage object for console or logging printout.
+        /// </summary>
+        /// <returns> A string representation of the ChatMessage object. </returns>
+        public override string ToString()
+        {
+            return $"ChatMessage(Role: {this.Role}, Content: \"{this.Content}\")";
+        }
+
+        /// <summary>
+        /// Write the content of this object to a JSON Writer.
+        /// </summary>
+        /// <param name="writer"> The JSON writer to be updated.</param>
+        void IUtf8JsonSerializable.Write(Utf8JsonWriter writer)
+        {
+            writer.WriteStartObject();
+            writer.WriteString("role", this.Role.ToString());
+            writer.WriteString("content", this.Content);
+            writer.WriteEndObject();
+        }
+
+        /// <summary>
+        /// Returns a new ChatMessage object representing the data read from the input JSON element.
+        /// </summary>
+        /// <param name="element"> The JSON element to deserialize. </param>
+        /// <returns> The deserialized ChatMessage object. </returns>
+        internal static ChatMessage DeserializeChatMessage(JsonElement element)
+        {
+            if (element.ValueKind == JsonValueKind.Null)
+            {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            ChatRole role = element.TryGetProperty("role", out JsonElement jsonRole)
+                ? new ChatRole(jsonRole.GetString())
+                : throw new Exception("Missing JSON `role` in `messages` element");
+
+            string content = element.TryGetProperty("content", out JsonElement jsonContent)
+                ? (jsonContent.GetString() ?? throw new Exception("Null `content` in `messages` element"))
+                : throw new Exception("Missing JSON `content` in `choices` element");
+
+            return new ChatMessage(role, content);
+        }
+    }
+}
