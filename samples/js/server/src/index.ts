591b33600fa85ebc1d61bfd9151a0e8301b5116b
Gerardo Lecaros <gerardo.lecaros.e@gmail.com>
2024-03-22 18:51:23 +0000

    Cleanup

--- samples/js/server/src/index.ts
+++ samples/js/server/src/index.ts
@@ -1,16 +1,17 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv";
 
-import chat from './routes/chat';
+import chat from "./routes/chat";
+import { ConfigParameter, getConfig } from "./config";
+
+dotenv.config();
 
 const app: Express = express();
-const port = 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello root World!');
-});
+const port = getConfig(ConfigParameter.port);
 
-app.use('/api/chat', chat);
+app.use("/api/chat", chat);
 
 app.listen(port, () => {
-    console.log(`[server]: Server is running at http://localhost:${port}`);
-  });
+  console.log(`[server]: Server is running at http://localhost:${port}`);
+});
