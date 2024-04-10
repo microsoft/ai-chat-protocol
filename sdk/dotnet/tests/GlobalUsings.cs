ad406ddfb635e672531719b21f84a989c8ad0a02
Gerardo Lecaros <gerardo.lecaros.e@gmail.com>
2024-04-10 13:05:24 -0700

    Temporarily removing C# client library. (#18)

--- sdk/dotnet/tests/GlobalUsings.cs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License. See LICENSE file in the project root for license information.
-
-#pragma warning disable SA1200 // Using directives should be placed correctly
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-#pragma warning restore SA1200 // Using directives should be placed correctly
