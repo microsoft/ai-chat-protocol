8cf19211bd92d05b8b6be2bf85fdf189ed4c59c3
Gerardo Lecaros <gerardo.lecaros.e@gmail.com>
2024-04-16 18:39:10 +0000

    Removing test server.

--- extra/test-server/Services/IMaaSClientProvider.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-namespace Azure.AI.Chat.SampleService.Services;
-
-public interface IMaaSClientProvider
-{
-    HttpClient GetClient();
-}
