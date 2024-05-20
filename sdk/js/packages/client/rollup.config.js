437eaa8de6478e0dc9682d0f2a02c056bc1b75b7
Gerardo Lecaros <10088504+glecaros@users.noreply.github.com>
2024-05-20 20:05:49 +0000

    format

--- sdk/js/packages/client/rollup.config.js
+++ sdk/js/packages/client/rollup.config.js
@@ -1,47 +1,56 @@
 import commonjs from "@rollup/plugin-commonjs";
 import dts from "rollup-plugin-dts";
-import typescript from '@rollup/plugin-typescript';
+import typescript from "@rollup/plugin-typescript";
 import nodeResolve from "@rollup/plugin-node-resolve";
 import alias from "@rollup/plugin-alias";
 
 export default [
   {
     input: "src/index.ts",
-    output: [{
-      file: "dist/cjs/index.js",
-      format: "cjs",
-      sourcemap: true
-    }, {
-      file: "dist/esm/index.js",
-      format: "esm",
-      sourcemap: true,
-    }],
+    output: [
+      {
+        file: "dist/cjs/index.js",
+        format: "cjs",
+        sourcemap: true,
+      },
+      {
+        file: "dist/esm/index.js",
+        format: "esm",
+        sourcemap: true,
+      },
+    ],
     plugins: [
       nodeResolve({
         preferBuiltins: true,
       }),
       commonjs(),
       typescript(),
-    ]
+    ],
   },
   {
     input: "src/index.ts",
-    output: [{
-      file: "dist/browser/index.js",
-      format: "esm",
-      sourcemap: true,
-    }, {
-      file: "dist/iife/index.js",
-      format: "iife",
-      name: "ChatProtocol",
-      sourcemap: true,
-    }],
+    output: [
+      {
+        file: "dist/browser/index.js",
+        format: "esm",
+        sourcemap: true,
+      },
+      {
+        file: "dist/iife/index.js",
+        format: "iife",
+        name: "ChatProtocol",
+        sourcemap: true,
+      },
+    ],
     plugins: [
       alias({
         entries: [
-          { find: "./util/stream.js", replacement: "./util/stream-browser.mjs" },
+          {
+            find: "./util/stream.js",
+            replacement: "./util/stream-browser.mjs",
+          },
           { find: "./util/url.js", replacement: "./util/url-browser.mjs" },
-        ]
+        ],
       }),
       typescript(),
       nodeResolve({
@@ -52,13 +61,13 @@ export default [
   },
   {
     input: "src/index.ts",
-    output: [{
-      file: "dist/index.d.ts",
-      format: "es",
-      sourcemap: true,
-    }],
-    plugins: [
-      dts(),
-    ]
+    output: [
+      {
+        file: "dist/index.d.ts",
+        format: "es",
+        sourcemap: true,
+      },
+    ],
+    plugins: [dts()],
   },
 ];
