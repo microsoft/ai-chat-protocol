2f92b235c4161e55b0a6489a00b2eba6c389e8d1
Gerardo Lecaros <10088504+glecaros@users.noreply.github.com>
2024-05-20 19:57:29 +0000

    Changing build system to Rollup.

--- sdk/js/packages/client/rollup.config.js
+++ sdk/js/packages/client/rollup.config.js
@@ -1,22 +1,64 @@
+import commonjs from "@rollup/plugin-commonjs";
 import dts from "rollup-plugin-dts";
+import typescript from '@rollup/plugin-typescript';
+import nodeResolve from "@rollup/plugin-node-resolve";
+import alias from "@rollup/plugin-alias";
 
 export default [
   {
-    input: ["dist/browser/index.js"],
-    plugins: [dts()],
-    output: {
-      format: "umd",
-      file: "dist/bundle/chat-protocol.umd.d.ts",
-      name: "ChatProtocol",
-    },
+    input: "src/index.ts",
+    output: [{
+      file: "dist/cjs/index.js",
+      format: "cjs",
+      sourcemap: true
+    }, {
+      file: "dist/esm/index.js",
+      format: "esm",
+      sourcemap: true,
+    }],
+    plugins: [
+      nodeResolve({
+        preferBuiltins: true,
+      }),
+      commonjs(),
+      typescript(),
+    ]
   },
   {
-    input: ["dist/browser/index.js"],
-    output: {
-      format: "umd",
+    input: "src/index.ts",
+    output: [{
+      file: "dist/browser/index.js",
+      format: "esm",
       sourcemap: true,
-      file: "dist/bundle/chat-protocol.umd.js",
+    }, {
+      file: "dist/iife/index.js",
+      format: "iife",
       name: "ChatProtocol",
-    },
+      sourcemap: true,
+    }],
+    plugins: [
+      alias({
+        entries: [
+          { find: "./util/stream.js", replacement: "./util/stream-browser.mjs" },
+          { find: "./util/url.js", replacement: "./util/url-browser.mjs" },
+        ]
+      }),
+      typescript(),
+      nodeResolve({
+        browser: true,
+        preferBuiltins: true,
+      }),
+    ],
+  },
+  {
+    input: "src/index.ts",
+    output: [{
+      file: "dist/index.d.ts",
+      format: "es",
+      sourcemap: true,
+    }],
+    plugins: [
+      dts(),
+    ]
   },
 ];
