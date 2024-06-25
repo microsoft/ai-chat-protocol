0449e879c2e30b98774ce74ed474fb3627c07e04
Gerardo Lecaros <10088504+glecaros@users.noreply.github.com>
2024-06-25 21:46:08 +0000

    added streaming

--- samples/backend/python/quart/__init__.py
+++ samples/backend/python/quart/__init__.py
@@ -4,9 +4,9 @@ import os
 import re
 from dotenv import load_dotenv
 from typing import Optional, Tuple
-from openai import AsyncAzureOpenAI, AzureOpenAI
+from openai import AsyncAzureOpenAI
 from azure.identity import DefaultAzureCredential, get_bearer_token_provider
-from quart import Quart, request, jsonify
+from quart import Quart, request, jsonify, stream_with_context
 from model import (
     AIChatRequest,
     AIChatCompletion,
@@ -14,6 +14,8 @@ from model import (
     AIChatRole,
     AIChatFile,
     AIChatError,
+    AIChatCompletionDelta,
+    AIChatMessageDelta,
 )
 
 load_dotenv()
@@ -23,6 +25,14 @@ AZURE_OPENAI_ENDPOINT = os.getenv("AZURE_OPENAI_ENDPOINT")
 AZURE_OPENAI_DEPLOYMENT = os.getenv("AZURE_OPENAI_DEPLOYMENT")
 AZURE_OPENAI_API_VERSION = os.getenv("AZURE_OPENAI_API_VERSION")
 
+token_provider = get_bearer_token_provider(
+    DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
+)
+client = AsyncAzureOpenAI(
+    api_version=AZURE_OPENAI_API_VERSION,
+    azure_endpoint=AZURE_OPENAI_ENDPOINT,
+    azure_ad_token_provider=token_provider,
+)
 
 app = Quart(__name__)
 
@@ -93,19 +103,10 @@ async def process_message():
             files = await request.files
             chat_request = reconstruct_multipart_request(form, files)
         elif request.content_type.startswith("application/json"):
-            chat_request = await request.get_json()
+            chat_request_dict = await request.get_json()
+            chat_request = AIChatRequest.from_dict(chat_request_dict)
         else:
             return jsonify({"error": "Unsupported Media Type"}), 415
-
-        token_provider = get_bearer_token_provider(
-            DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default"
-        )
-        client = AsyncAzureOpenAI(
-            api_version=AZURE_OPENAI_API_VERSION,
-            azure_endpoint=AZURE_OPENAI_ENDPOINT,
-            azure_ad_token_provider=token_provider,
-        )
-
         completion = await client.chat.completions.create(
             model=AZURE_OPENAI_DEPLOYMENT,
             messages=[to_openai_message(message) for message in chat_request.messages],
@@ -120,8 +121,60 @@ async def process_message():
         )
         return jsonify(response.to_dict())
     except Exception as e:
-        error = AIChatError(code="internal_error", message=str(e))
-        return jsonify(AIChatError(error=error).to_dict()), 500
+        return (
+            jsonify(AIChatError(code="internal_error", message=str(e)).to_dict()),
+            500,
+        )
+
+
+def object_to_json_line(obj):
+    return f"{json.dumps(obj.to_dict())}\r\n"
+
+
+@app.route("/api/chat/stream", methods=["POST"])
+async def process_message_stream():
+    @stream_with_context
+    async def async_generator():
+        try:
+            if request.content_type.startswith("multipart/form-data"):
+                form = await request.form
+                files = await request.files
+                chat_request = reconstruct_multipart_request(form, files)
+            elif request.content_type.startswith("application/json"):
+                chat_request = await request.get_json()
+            else:
+                yield object_to_json_line(
+                    AIChatError(
+                        code="unsupported_media_type", message="Unsupported Media Type"
+                    )
+                )
+                return
+
+            stream = await client.chat.completions.create(
+                model=AZURE_OPENAI_DEPLOYMENT,
+                stream=True,
+                messages=[
+                    to_openai_message(message) for message in chat_request.messages
+                ],
+            )
+
+            async for chunk in stream:
+                if len(chunk.choices) == 0:
+                    continue
+                delta = chunk.choices[0].delta
+                response_chunk = AIChatCompletionDelta(
+                    delta=AIChatMessageDelta(
+                        content=delta.content,
+                        role=delta.role,
+                    )
+                )
+                yield object_to_json_line(response_chunk)
+
+        except Exception as e:
+            error = AIChatError(code="internal_error", message=str(e))
+            yield object_to_json_line(error)
+
+    return async_generator(), 200, {"Content-Type": "application/jsonl"}
 
 
 if __name__ == "__main__":
